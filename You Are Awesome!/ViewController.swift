cae668da3868c4bf3dba5012de494a5a17f29ebc
Dom Ronan <dominickronan@doms-macbook-pro.local>
2017-01-29 01:03:52 -0500

    Random generator

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var messageButton: UIButton!
+    
+    var lastIndex = -1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        
+        let messages = ["You Are Fantastic!",
+                        "You Are Great!",
+                        "You Are Amazing!",
+                        "You Are Awesome!",
+                        "When the Genius Bar needs help, they call you!",
+                        "You Brighten My Day!",
+                        "I Can't Wait to Use Your App!",
+                        "You Are Da Bomb"]
+        
+        var randomIndex: Int
+       
+        repeat {
+           randomIndex = Int(arc4random_uniform(UInt32(messages.count)))
+        } while randomIndex == lastIndex
+        messageLabel.text = messages[randomIndex]
+    
+    lastIndex = randomIndex
+
+        
+        /*
+        messageLabel.text = messages[index]
+        if index == messages.count - 1 {
+            index = 0
+        } else {
+           index = index + 1
+        }
+      */
+        /*
+        let message1 = "You Are Fantastic!!!"
+        let message2 = "You Are Great!"
+        let message3 = "You Are Amazing!"
+        
+        if messageLabel.text == message1 {
+            messageLabel.text = message2
+        } else if messageLabel.text == message2 {
+            messageLabel.text = message3
+        } else {
+            messageLabel.text = message1
+        }
+ */
+    }
 
 }
-
